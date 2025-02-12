969d247ec54a0a005921cfd4eba56971b197b0d1
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2025-02-12 23:25:57 +0500

    version 1.5

--- src/pages/AboutPage/AboutUs.jsx
+++ src/pages/AboutPage/AboutUs.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function AboutUs() {
   return (
-    <div>
+    <div className='min-h-screen mx-auto overflow-hidden'>
+
+      <div className='h-[40vh] py-40 w-full text-center bg-blue-600 rounded-b-[100%] space-y-5 text-white'>
+
+        <h1 className='text-[2.5rem] font-semibold'>About Us</h1>
+       
+        <p className='text-sm'><Link to='/' className=' hover:underline'> Home</Link> / About Us</p>
+
+      </div>
       
+      <div className='max-w-screen-2xl'>
+      <div className='border-2 my-14 '>
+          
+          <div>
+
+            
+          
+
+          </div>
+         
+          <div>
+             
+             <h4 className='font-medium'>ABOUT STELLAR</h4>
+
+
+          </div>
+          
+          
+      </div>
+      </div>
+
     </div>
   )
 }
