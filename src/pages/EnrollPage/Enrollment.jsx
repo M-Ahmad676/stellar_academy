cac9376c971b553210d0e77a234822df8e092373
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2025-03-19 22:58:11 +0500

    update 1.8.4: Apply now Form Updated

--- src/pages/EnrollPage/Enrollment.jsx
+++ src/pages/EnrollPage/Enrollment.jsx
@@ -4,6 +4,7 @@ import ArrowList from "../../components/ArrowList/ArrowList";
 import LazyloadingImage from '../../components/LazyLoadingImage'
 import { useForm } from "react-hook-form";
 import LazyLoadingImage from "../../components/LazyLoadingImage";
+import { option } from "framer-motion/client";
 
 export default function Enrollment() {
   const Instructions = [
@@ -17,17 +18,21 @@ export default function Enrollment() {
   const [placeholder, setPlaceHolder] = useState({
      
     fullName: "Full Name",
-    DOB: "Date of Birth", 
+    Gender: "Gender",
     email: "Email",
     whatsAppNo: 'WhatsApp Number',
     Grade: "Grade",
     Subjects: "Enter Subjects"
   })
 
+  const [selectGrade, setSelectGrade] = useState('')
+  const grades = ["9th", "10th", "11th", "12th"];
+
   const {
     register,
     handleSubmit,
     reset,
+    watch,
     formState: {errors}, 
   } = useForm()
 
@@ -63,10 +68,53 @@ export default function Enrollment() {
           </div>
 
           <div>
-             
+            <select {...register("gender",{required:"Gender is required"})} className="w-full">
+              <option value="">Select Gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>             
+            {
+              errors.gender && (
+               <p>{errors.gender.message}</p>
+              )
+            }
+          </div>
+          <div>
+           <input type="email" {...register("email", {required:"Email is required"})} 
+           className="w-full border-b-2 focus:outline-none border-gray-300"/>
+           {
+            errors.email && (
+              <p>{errors.email.message}</p>
+            )
+           }
+          </div>
+          <div>
+             <input type="tel" {...register("whatsapp",{required:"WhatsApp Number is required"})}
+             className="w-full border-b-2 border-gray-300 focus:outine-none" />
+             {
+              errors.whatsapp && (
+                <p>{errors.whatsapp.message}</p>
+              )
+             }
+          </div>
+          <div>
+            <select {...register("grade",{required:"Grade is required"})}
+            className="w-full"
+            onChange={(e) => setSelectGrade(e.target.value)}
+            >
+            <option value="">Select Grade</option>
+            {grades.map((grade) => (
+              <option key={grade} value={grade}>{grade}</option>
 
+            ))}
+            </select>
+             {errors.grade && (
+                <p>{errors.grade.message}</p>
+             )}
+          </div>
 
-
+          <div>
+            
           </div>
       
         </form> 
